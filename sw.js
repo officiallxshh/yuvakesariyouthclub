57d8bfaba96bc0fab323951827b65895cf5b27e8
officiallxshh <officiallxshh@gmail.com>
2026-09-19 20:10:52 +0530

    chore: refresh cache for temple image update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yyc-v14-temple-feature';
+const CACHE='yyc-v15-temple-image';
 const ASSETS=[
   './','./index.html','./styles.css','./app.js',
   './assets/yyc-logo-clean.webp','./assets/yyc-logo.webp','./assets/hero-tulunad.webp',
